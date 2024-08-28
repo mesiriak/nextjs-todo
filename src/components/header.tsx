80951a7118c30392e4eed5dc47e21f7f55b954aa
Max Zhuravskyi <m.zhuravskyi@treeum.net>
2024-08-28 05:37:28 +0300

    feat: v1 version.

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+
+
+export const Header = () => {
+    return (
+        <header className={'bg-black text-white mb-3'}>
+            <div id={'app-title'} className={'text-xl'}>Simple TODO Application</div>
+        </header>
+    )
+}
